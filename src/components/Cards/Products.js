9950adbf450a96ab1b9526a7d50e763a99b06b27
Haahns <jakob.hahnsson@gmail.com>
2022-02-17 11:57:57 +0200

    delete funktion ok

--- src/components/Cards/Products.js
+++ src/components/Cards/Products.js
@@ -1,47 +1,88 @@
 import React, { useEffect, useState } from "react";
 import Style from "./Cards.module.css";
-import checkJWT from '../../utils/helpers';
-import { Grid, Drawer, Typography, Button, Box, Card } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import checkJWT from "../../utils/helpers"; 
+import { Grid, Drawer, Typography, Button, Box, Card } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import DisplayProducts from "./productsComponents/DisplayProducts";
 import DisplayOneProduct from "./productsComponents/DisplayOneProduct";
 
 export default function Products() {
-
   const [displayOne, setDisplayOne] = useState(false);
-  const navigate = useNavigate()
-  const [products, setProducts] = useState([])
+  const navigate = useNavigate();
+  const [products, setProducts] = useState([]);
   const [productToDisplay, setProductToDisplay] = useState({});
 
   useEffect(async () => {
-
     let jwt = checkJWT();
 
     const requestOptions = {
       headers: {
-        Authorization: `Bearer ${jwt}`
-      }
+        Authorization: `Bearer ${jwt}`,
+      },
     };
-    setProducts(await axios.get(`https://cna22-products-service.herokuapp.com/products`, requestOptions).then((res) => res?.data.items));
+    setProducts(
+      await axios
+        .get(
+          `https://cna22-products-service.herokuapp.com/products`,
+          requestOptions
+        )
+        .then((res) => res?.data.items)
+    );
     console.log(products);
-  }, [])
+  }, []);
 
   useEffect(async () => {
     console.log(products);
-  }, [products])
+  }, [products]);
   return (
-    <Card className={Style.card} style={{height: '750px'}}>
-      {displayOne ? <DisplayOneProduct goBack={()=>{setDisplayOne(false)}} name={productToDisplay.name} description={productToDisplay.description} manufacturer={productToDisplay.manufacturer} picUrl={productToDisplay.imageURLs[0]} price={productToDisplay.price} chip={productToDisplay.chip} memory={productToDisplay.memory}  rating={productToDisplay.rating} packageDimensionsWidth={productToDisplay.packageDimensions.width} packageDimensionsHeight={productToDisplay.packageDimensions.height} packageDimensionsDepth={productToDisplay.packageDimensions.depth}/> :
+    <Card className={Style.card} style={{ height: "750px" }}>
+      {displayOne ? (
+        <DisplayOneProduct
+          goBack={() => {
+            setDisplayOne(false);
+          }}
+          name={productToDisplay.name}
+          description={productToDisplay.description}
+          manufacturer={productToDisplay.manufacturer}
+          picUrl={productToDisplay.imageURLs[0]}
+          price={productToDisplay.price}
+          chip={productToDisplay.chip}
+          memory={productToDisplay.memory}
+          rating={productToDisplay.rating}
+          packageDimensionsWidth={productToDisplay.packageDimensions.width}
+          packageDimensionsHeight={productToDisplay.packageDimensions.height}
+          packageDimensionsDepth={productToDisplay.packageDimensions.depth}
+          pid={productToDisplay.pid}
+        />
+      ) : (
         <>
           <Typography variant="h5">Products</Typography> <br />
-          <Grid container spacing={2} justifyContent={'center'} alignItems={'center'}>
-            {products.length > 0 ? products.map((e) => {
-              return <DisplayProducts onClick={()=>{setProductToDisplay(e); setDisplayOne(true); console.log()}} name={e.name} pPrice={e.price} pPhoto={e.imageURLs[0]} />
-            }) : null}
+          <Grid
+            container
+            spacing={2}
+            justifyContent={"center"}
+            alignItems={"center"}
+          >
+            {products.length > 0
+              ? products.map((e) => {
+                  return (
+                    <DisplayProducts
+                      onClick={() => {
+                        setProductToDisplay(e);
+                        setDisplayOne(true);
+                        console.log();
+                      }}
+                      name={e.name}
+                      pPrice={e.price}
+                      pPhoto={e.imageURLs[0]}
+                    />
+                  );
+                })
+              : null}
           </Grid>
         </>
-      }
+      )}
     </Card>
   );
-};
+}
--- src/components/Cards/productsComponents/DisplayOneProduct.js
+++ src/components/Cards/productsComponents/DisplayOneProduct.js
@@ -1,55 +1,159 @@
-import React, { useRef } from 'react';
-import { Card, TextField, Button } from '@mui/material'
-import { useNavigate } from 'react-router';
+import React, { useRef } from "react";
+import { Card, TextField, Button } from "@mui/material";
+import { useNavigate } from "react-router";
+import axios from 'axios';
+import checkJWT from "../../../utils/helpers"; 
+
+
 
 function DisplayOneProduct(props) {
-  const { goBack,name, description, manufacturer, price, chip, memory, rating, picUrl, packageDimensionsWidth, packageDimensionsHeight, packageDimensionsDepth } = props
-  console.log(props)
+  const {
+    goBack,
+    name,
+    pid,
+    description,
+    manufacturer,
+    price,
+    chip,
+    memory,
+    rating,
+    picUrl,
+    packageDimensionsWidth,
+    packageDimensionsHeight,
+    packageDimensionsDepth,
+  } = props;
+  console.log(props);
 
-  const nameRef = useRef()
-  const descriptionRef = useRef()
-  const manufacturerRef = useRef()
-  const priceRef = useRef()
-  const chipRef = useRef()
-  const memoryRef = useRef()
-  const ratingRef = useRef()
-  const picUrlRef = useRef()
-  const packageDimensionsWidthRef = useRef()
-  const packageDimensionsHeightRef = useRef()
-  const packageDimensionsDepthRef = useRef()
+  const nameRef = useRef();
+  const descriptionRef = useRef();
+  const manufacturerRef = useRef();
+  const priceRef = useRef();
+  const chipRef = useRef();
+  const memoryRef = useRef();
+  const ratingRef = useRef();
+  const picUrlRef = useRef();
+  const packageDimensionsWidthRef = useRef();
+  const packageDimensionsHeightRef = useRef();
+  const packageDimensionsDepthRef = useRef();
+
+  async function deleteProduct() {
+    console.log(pid);
+
+    let jwt = checkJWT();
+
+    const requestOptions = {
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    };
+
+    await axios
+      .delete(
+        `https://cna22-products-service.herokuapp.com/product/${pid}`,
+        requestOptions
+      )  }
   return (
-
     <>
-      <div style={{ height: '250px' }}>
-        {picUrl ? (<><img src={picUrl} width="250" height="250" />
-          <br /></>) : (<><p>No image found</p> <br /></>)}
+      <div style={{ height: "250px" }}>
+        {picUrl ? (
+          <>
+            <img src={picUrl} width="250" height="250" />
+            <br />
+          </>
+        ) : (
+          <>
+            <p>No image found</p> <br />
+          </>
+        )}
         <label>Name: </label>
-        <TextField inputRef={nameRef} size="small" type="text" value={name} /> <br />
+        <TextField
+          inputRef={nameRef}
+          size="small"
+          type="text"
+          value={name}
+        />{" "}
+        <br />
         <label>Description: </label>
-        <TextField inputRef={descriptionRef} size="small" type="text" value={description} /><br />
+        <TextField
+          inputRef={descriptionRef}
+          size="small"
+          type="text"
+          value={description}
+        />
+        <br />
         <label>Manufacturer: </label>
-        <TextField inputRef={manufacturerRef} size="small" type="text" value={manufacturer} /><br />
+        <TextField
+          inputRef={manufacturerRef}
+          size="small"
+          type="text"
+          value={manufacturer}
+        />
+        <br />
         <label>price: </label>
-        <TextField inputRef={priceRef} size="small" type="text" value={price} /><br />
+        <TextField inputRef={priceRef} size="small" type="text" value={price} />
+        <br />
         <label>chip: </label>
-        <TextField inputRef={chipRef} size="small" type="text" value={chip} /><br />
+        <TextField inputRef={chipRef} size="small" type="text" value={chip} />
+        <br />
         <label>memory: </label>
-        <TextField inputRef={memoryRef} size="small" type="text" value={memory} /><br />
+        <TextField
+          inputRef={memoryRef}
+          size="small"
+          type="text"
+          value={memory}
+        />
+        <br />
         <label>rating: </label>
-        <TextField inputRef={ratingRef} size="small" type="text" value={rating} /><br />
+        <TextField
+          inputRef={ratingRef}
+          size="small"
+          type="text"
+          value={rating}
+        />
+        <br />
         <label>picUrl: </label>
-        <TextField inputRef={picUrlRef} size="small" type="text" value={picUrl} /><br />
+        <TextField
+          inputRef={picUrlRef}
+          size="small"
+          type="text"
+          value={picUrl}
+        />
+        <br />
         <label>packageDimensionsWidth: </label>
-        <TextField inputRef={packageDimensionsWidthRef} size="small" type="text" value={packageDimensionsWidth} /><br />
+        <TextField
+          inputRef={packageDimensionsWidthRef}
+          size="small"
+          type="text"
+          value={packageDimensionsWidth}
+        />
+        <br />
         <label>packageDimensionsHeight: </label>
-        <TextField inputRef={packageDimensionsHeightRef} size="small" type="text" value={packageDimensionsHeight} /><br />
+        <TextField
+          inputRef={packageDimensionsHeightRef}
+          size="small"
+          type="text"
+          value={packageDimensionsHeight}
+        />
+        <br />
         <label>packageDimensionsDepth: </label>
-        <TextField inputRef={packageDimensionsDepthRef} size="small" type="text" value={packageDimensionsDepth} /><br />
-        <Button onClick={goBack} color="error" variant="outlined">Go Back</Button>
-        <Button color="success" variant="outlined">Save</Button>
+        <TextField
+          inputRef={packageDimensionsDepthRef}
+          size="small"
+          type="text"
+          value={packageDimensionsDepth}
+        />
+        <br />
+        <Button onClick={goBack} color="primary" variant="outlined">
+          Go Back
+        </Button>
+        <Button color="success" variant="outlined">
+          Save
+        </Button>
+        <Button onClick={deleteProduct} color="error" variant="outlined">
+          Delete
+        </Button>
       </div>
     </>
-
   );
 }
 
